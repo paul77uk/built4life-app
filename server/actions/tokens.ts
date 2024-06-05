4088d80882b85373dcc806cba552b4dea2daf280
paul77uk <paul77uk@gmail.com>
2024-06-05 22:26:26 +0100

    added magic link

--- server/actions/tokens.ts
+++ server/actions/tokens.ts
@@ -1,170 +1,170 @@
-"use server";
+// "use server";
 
-import { eq } from "drizzle-orm";
-import { db } from "..";
-import {
-  emailTokens,
-  passwordResetTokens,
-  twoFactorTokens,
-  users,
-} from "../schema";
-import crypto from "crypto";
+// import { eq } from "drizzle-orm";
+// import { db } from "..";
+// import {
+//   emailTokens,
+//   passwordResetTokens,
+//   twoFactorTokens,
+//   users,
+// } from "../schema";
+// import crypto from "crypto";
 
-export const getVerificationTokenByEmail = async (email: string) => {
-  try {
-    const verificationToken = await db.query.emailTokens.findFirst({
-      where: eq(emailTokens.token, email),
-    });
-    return verificationToken;
-  } catch (error) {
-    return null;
-  }
-};
+// export const getVerificationTokenByEmail = async (email: string) => {
+//   try {
+//     const verificationToken = await db.query.emailTokens.findFirst({
+//       where: eq(emailTokens.token, email),
+//     });
+//     return verificationToken;
+//   } catch (error) {
+//     return null;
+//   }
+// };
 
-export const generateEmailVerificationToken = async (email: string) => {
-  const token = crypto.randomUUID();
-  const expires = new Date(new Date().getTime() + 3600 * 1000);
+// export const generateEmailVerificationToken = async (email: string) => {
+//   const token = crypto.randomUUID();
+//   const expires = new Date(new Date().getTime() + 3600 * 1000);
 
-  const existingToken = await getVerificationTokenByEmail(email);
+//   const existingToken = await getVerificationTokenByEmail(email);
 
-  if (existingToken) {
-    await db.delete(emailTokens).where(eq(emailTokens.id, existingToken.id));
-  }
+//   if (existingToken) {
+//     await db.delete(emailTokens).where(eq(emailTokens.id, existingToken.id));
+//   }
 
-  const verificationToken = await db
-    .insert(emailTokens)
-    .values({
-      email,
-      token,
-      expires,
-    })
-    .returning();
-  return verificationToken;
-};
+//   const verificationToken = await db
+//     .insert(emailTokens)
+//     .values({
+//       email,
+//       token,
+//       expires,
+//     })
+//     .returning();
+//   return verificationToken;
+// };
 
-export const newVerification = async (token: string) => {
-  const existingToken = await getVerificationTokenByEmail(token);
-  if (!existingToken) return { error: "Token not found" };
-  const hasExpired = new Date(existingToken.expires) < new Date();
+// export const newVerification = async (token: string) => {
+//   const existingToken = await getVerificationTokenByEmail(token);
+//   if (!existingToken) return { error: "Token not found" };
+//   const hasExpired = new Date(existingToken.expires) < new Date();
 
-  if (hasExpired) return { error: "Token has expired" };
+//   if (hasExpired) return { error: "Token has expired" };
 
-  const existingUser = await db.query.users.findFirst({
-    where: eq(users.email, existingToken.email),
-  });
-  if (!existingUser) return { error: "Email does not exist" };
-  await db.update(users).set({
-    emailVerified: new Date(),
-    email: existingToken.email,
-  });
+//   const existingUser = await db.query.users.findFirst({
+//     where: eq(users.email, existingToken.email),
+//   });
+//   if (!existingUser) return { error: "Email does not exist" };
+//   await db.update(users).set({
+//     emailVerified: new Date(),
+//     email: existingToken.email,
+//   });
 
-  await db.delete(emailTokens).where(eq(emailTokens.id, existingToken.id));
-  return { success: "Email verified" };
-};
+//   await db.delete(emailTokens).where(eq(emailTokens.id, existingToken.id));
+//   return { success: "Email verified" };
+// };
 
-export const getPasswordResetTokenByToken = async (token: string) => {
-  try {
-    const passwordResetToken = await db.query.passwordResetTokens.findFirst({
-      where: eq(passwordResetTokens.token, token),
-    });
-    return passwordResetToken;
-  } catch {
-    return null;
-  }
-};
+// export const getPasswordResetTokenByToken = async (token: string) => {
+//   try {
+//     const passwordResetToken = await db.query.passwordResetTokens.findFirst({
+//       where: eq(passwordResetTokens.token, token),
+//     });
+//     return passwordResetToken;
+//   } catch {
+//     return null;
+//   }
+// };
 
-export const getPasswordResetTokenByEmail = async (email: string) => {
-  try {
-    const passwordResetToken = await db.query.passwordResetTokens.findFirst({
-      where: eq(passwordResetTokens.email, email),
-    });
-    return passwordResetToken;
-  } catch {
-    return null;
-  }
-};
+// export const getPasswordResetTokenByEmail = async (email: string) => {
+//   try {
+//     const passwordResetToken = await db.query.passwordResetTokens.findFirst({
+//       where: eq(passwordResetTokens.email, email),
+//     });
+//     return passwordResetToken;
+//   } catch {
+//     return null;
+//   }
+// };
 
-export const getTwoFactorTokenByEmail = async (email: string) => {
-  try {
-    const twoFactorToken = await db.query.twoFactorTokens.findFirst({
-      where: eq(twoFactorTokens.email, email),
-    });
-    return twoFactorToken;
-  } catch {
-    return null;
-  }
-};
+// export const getTwoFactorTokenByEmail = async (email: string) => {
+//   try {
+//     const twoFactorToken = await db.query.twoFactorTokens.findFirst({
+//       where: eq(twoFactorTokens.email, email),
+//     });
+//     return twoFactorToken;
+//   } catch {
+//     return null;
+//   }
+// };
 
 
-export const getTwoFactorTokenByToken = async (token: string) => {
-  try {
-    const twoFactorToken = await db.query.twoFactorTokens.findFirst({
-      where: eq(twoFactorTokens.token, token),
-    });
-    return twoFactorToken;
-  } catch {
-    return null;
-  }
-};
+// export const getTwoFactorTokenByToken = async (token: string) => {
+//   try {
+//     const twoFactorToken = await db.query.twoFactorTokens.findFirst({
+//       where: eq(twoFactorTokens.token, token),
+//     });
+//     return twoFactorToken;
+//   } catch {
+//     return null;
+//   }
+// };
 
-export const generatePasswordResetToken = async (email: string) => {
-  try {
-    const token = crypto.randomUUID();
-    const expires = new Date(new Date().getTime() + 3600 * 1000);
+// export const generatePasswordResetToken = async (email: string) => {
+//   try {
+//     const token = crypto.randomUUID();
+//     const expires = new Date(new Date().getTime() + 3600 * 1000);
 
-    const existingToken = await getPasswordResetTokenByEmail(email);
+//     const existingToken = await getPasswordResetTokenByEmail(email);
 
-    if (existingToken) {
-      await db
-        .delete(passwordResetTokens)
-        .where(eq(passwordResetTokens.id, existingToken.id));
-    }
+//     if (existingToken) {
+//       await db
+//         .delete(passwordResetTokens)
+//         .where(eq(passwordResetTokens.id, existingToken.id));
+//     }
 
-    const passwordResetToken = await db
-      .insert(passwordResetTokens)
-      .values({
-        email,
-        token,
-        expires,
-      })
-      .returning();
-    return passwordResetToken;
-  } catch (e) {
-    return null;
-  }
-};
+//     const passwordResetToken = await db
+//       .insert(passwordResetTokens)
+//       .values({
+//         email,
+//         token,
+//         expires,
+//       })
+//       .returning();
+//     return passwordResetToken;
+//   } catch (e) {
+//     return null;
+//   }
+// };
 
-// the database won't send the data back to you once you insert it
-// so we need to add the returning() method to get the data back
-// otherwise the return passwordResetToken will be undefined
+// // the database won't send the data back to you once you insert it
+// // so we need to add the returning() method to get the data back
+// // otherwise the return passwordResetToken will be undefined
 
-// the returning() method is a drizzle-orm method that returns the data that was inserted
+// // the returning() method is a drizzle-orm method that returns the data that was inserted
 
-// The RETURNING clause in SQL is used to retrieve values from a data modification statement, such as INSERT , UPDATE , or DELETE after the statement has been executed. This can be particularly useful when we want to know the values that were actually inserted, updated, or deleted as a result of the operation.
+// // The RETURNING clause in SQL is used to retrieve values from a data modification statement, such as INSERT , UPDATE , or DELETE after the statement has been executed. This can be particularly useful when we want to know the values that were actually inserted, updated, or deleted as a result of the operation.
 
-export const generateTwoFactorToken = async (email: string) => {
-  try {
-    const token = crypto.randomInt(100_000, 1_000_000).toString();
-    const expires = new Date(new Date().getTime() + 3600 * 1000);
+// export const generateTwoFactorToken = async (email: string) => {
+//   try {
+//     const token = crypto.randomInt(100_000, 1_000_000).toString();
+//     const expires = new Date(new Date().getTime() + 3600 * 1000);
 
-    const existingToken = await getTwoFactorTokenByEmail(email);
+//     const existingToken = await getTwoFactorTokenByEmail(email);
 
-    if (existingToken) {
-      await db
-        .delete(twoFactorTokens)
-        .where(eq(twoFactorTokens.id, existingToken.id));
-    }
+//     if (existingToken) {
+//       await db
+//         .delete(twoFactorTokens)
+//         .where(eq(twoFactorTokens.id, existingToken.id));
+//     }
 
-    const twofactorToken = await db
-      .insert(twoFactorTokens)
-      .values({
-        email,
-        token,
-        expires,
-      })
-      .returning();
-    return twofactorToken;
-  } catch (e) {
-    return null;
-  }
-};
+//     const twofactorToken = await db
+//       .insert(twoFactorTokens)
+//       .values({
+//         email,
+//         token,
+//         expires,
+//       })
+//       .returning();
+//     return twofactorToken;
+//   } catch (e) {
+//     return null;
+//   }
+// };
