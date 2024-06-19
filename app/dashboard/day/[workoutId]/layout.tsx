5e585bed438ff2e056e6a16efa8b49d672cb8d2d
paul77uk <paul77uk@gmail.com>
2024-06-19 18:24:46 +0100

    added accordion to weeks and days

--- app/dashboard/day/[workoutId]/layout.tsx
+++ app/dashboard/day/[workoutId]/layout.tsx
@@ -36,30 +36,28 @@ export default async function DayLayout({
       <div className="flex flex-col gap-3">
         <Accordion type="single" collapsible className="w-full">
           {weeksData.map((week) => (
-            <AccordionItem value={week.id}>
-              <div key={week.id}>
-                <AccordionTrigger className="flex gap-1 bg-primary py-1.5 px-3 rounded w-full">
-                  <div>Week</div> {week.number}
-                </AccordionTrigger>
-                <div className="flex flex-col my-1">
-                  {week.days.map((day) => (
-                    <AccordionContent>
-                      <div key={day.id}>
-                        {/* <DayBtn dayNumber={day.number} dayId={day.id} /> */}
-                        <Link
-                          href={`/dashboard/day/${workoutId}/exercise/${day.id}`}
-                        >
-                          <DayBtn day={day} workoutId={workoutId} />
-                        </Link>
-                        {/* <ul>
+            <AccordionItem value={week.id} key={week.id}>
+              <AccordionTrigger className="flex gap-1 bg-primary py-1.5 px-3 rounded w-full">
+                <div>Week</div> {week.number}
+              </AccordionTrigger>
+              <div className="flex flex-col my-1">
+                {week.days.map((day) => (
+                  <AccordionContent>
+                    <div key={day.id}>
+                      {/* <DayBtn dayNumber={day.number} dayId={day.id} /> */}
+                      <Link
+                        href={`/dashboard/day/${workoutId}/exercise/${day.id}`}
+                      >
+                        <DayBtn day={day} workoutId={workoutId} />
+                      </Link>
+                      {/* <ul>
                     {day.exercises.map((exercise) => (
                       <li key={exercise.id}>{exercise.name}</li>
                     ))}
                   </ul> */}
-                      </div>
-                    </AccordionContent>
-                  ))}
-                </div>
+                    </div>
+                  </AccordionContent>
+                ))}
               </div>
             </AccordionItem>
           ))}
