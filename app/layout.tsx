076966201af40b5160ef0820713c29ff8aef5738
paul77uk <paul77uk@gmail.com>
2024-06-23 00:15:51 +0100

    test favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,10 @@ import Toast from "@/components/ui/toast";
 import QueryProvider from "@/components/providers/query-provider";
 import { cn } from "@/lib/utils";
 
-const roboto = Roboto({ weight: "400", preload: false});
+const roboto = Roboto({ weight: "400", preload: false });
 
 export const metadata: Metadata = {
+  icons: ["favicon.ico"],
   title: "Built 4 Life",
   description: "Workout tracking app for the fitness enthusiast.",
 };
@@ -22,7 +23,12 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={cn(roboto.className, "bg-[#1E1E1E]")}>
-        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          forcedTheme="dark"
+        >
           <Nav />
           <Toast />
           <QueryProvider>{children}</QueryProvider>
